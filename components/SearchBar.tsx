14284ce3bf4ed1dae3a03a3f14b3b51a28c44f87
n8zzle <playwithn8zzle@gmail.com>
2024-07-05 20:21:10 +0200

    headlessui, and manufacturer filtering

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,18 @@
 "use client";
+
+import { useState } from "react";
+import SearchManufacturer from "./SearchManufacturer";
+
 const SearchBar = () => {
   const handleSearch = () => {};
+  const [manufacturer, setManufacturer] = useState("");
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
-        <SearchManufacturer />
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
       </div>
     </form>
   );
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,7 +1,79 @@
+"use client";
+import { manufacturers } from "@/constants";
+import { SearchManufacturerProps } from "@/types";
+import { Combobox, Transition } from "@headlessui/react";
+import Image from "next/image";
+import React, { useState, Fragment } from "react";
 
-type Props = {};
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, "")),
+        );
 
-const SearchManufacturer = (props: Props) => {
-  return SearchManufacturer
+  return (
+    <div className="serch-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
 
-export default SearchManufacturer
+          <Transition
+            as={Fragment}
+            leave="transition ease0in duraiton-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options>
+              {filteredManufacturers.length === 0 && query !== "" ? (
+                <Combobox.Option
+                  value={query}
+                  className={"search-manufacturer__opiton"}
+                >
+                  Create "{query}"
+                </Combobox.Option>
+              ) : (
+                filteredManufacturers.map((item) => (
+                  <Combobox.Option
+                    key={item}
+                    className={({ active }) =>
+                      `relative search-manufacturer__opiton ${active ? `bg-primary-blue text-white` : `text-gray-900`}`
+                    }
+                    value={item}
+                  >
+                    {item}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+};
+
+export default SearchManufacturer;
