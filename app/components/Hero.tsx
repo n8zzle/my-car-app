c38927cbef418e3a34f6812be22576a38140a0fa
n8zzle <playwithn8zzle@gmail.com>
2024-07-05 15:44:20 +0200

    Preparing project (deleting useless files)

---
+++ app/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const Hero = (props: Props) => {
+  return (
+    <div className="max-w-7xl mx-auto bg-red-100">
+      <div className="p-5 m-5">
+        <h1 className="text-4xl font-bold">Carhub</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
